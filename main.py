82166723925c09bcf1de1c38c860f36e64bf608e
jasper_hansen <jasper.hansen@enovetic.de>
2024-09-29 17:48:00 +0200

    Final commit2

--- main.py
+++ main.py
@@ -11,9 +11,8 @@ window.minsize(width=300, height=200)
 # Functions
 
 def button_clicked():
-    value = input.get()
-    resultLabel["text"] = (round(float(value)/1.609344, 2))
-
+    value = miles_input.get()
+    result_label["text"] = (round(float(value) / 1.609344, 2))
 
 
 # Widgets
@@ -22,26 +21,25 @@ def button_clicked():
 empty = Label(text="")
 empty.grid(column=0, row=0)
 
-miles = Label(text="Miles")
-miles.grid(column=2, row=1)
+miles_Label = Label(text="Miles")
+miles_Label.grid(column=2, row=1)
 
-label1 = Label(text="is equal to")
-label1.grid(column=0, row=2)
+is_equal_to_label = Label(text="is equal to")
+is_equal_to_label.grid(column=0, row=2)
 
-resultLabel = Label(text="0")
-resultLabel.grid(column=1, row=2)
+result_label = Label(text="0")
+result_label.grid(column=1, row=2)
 
-label2 = Label(text="Kilometers")
-label2.grid(column=2, row=2)
+kilometers_label = Label(text="Kilometers")
+kilometers_label.grid(column=2, row=2)
 
 # Entry
 
-input = Entry(width=10)
-input.grid(column=1, row=1)
+miles_input = Entry(width=10)
+miles_input.grid(column=1, row=1)
 
 # Button
 calculate_button = Button(text="Calculate", command=button_clicked)
 calculate_button.grid(column=1, row=3)
-
 
 window.mainloop()
